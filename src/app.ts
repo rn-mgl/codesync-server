fe28d24dbe3999e2b4b026c5dd0ab344d557a858
rn-mgl <reinb2ra@gmail.com>
2026-04-04 20:52:19 +0800

    increase limit

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
