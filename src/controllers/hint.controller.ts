4d19c823823d11d5943fafdeff4a7187d0a861f8
rn-mgl <reinb2ra@gmail.com>
2026-05-19 20:10:08 +0800

    get all hints

--- src/controllers/hint.controller.ts
+++ src/controllers/hint.controller.ts
@@ -3,21 +3,18 @@ import {
   isValidCreateHintPayload,
   isValidUpdateHintPayload,
 } from "@src/guard/hint.guard";
-import type {
-  AdditionalHintData,
-  BaseHintData,
-} from "@src/interface/hint.interface";
+import type { BaseHintData } from "@src/interface/hint.interface";
+import type { BaseProblemData } from "@src/interface/problem.interface";
 import Hint from "@src/models/hint.model";
+import Problem from "@src/models/problem.model";
 import { buildHintPayload } from "@src/services/hint.service";
 import { getProblemByLookup } from "@src/services/problem.service";
 import {
-  assignField,
-  isAdditionalHintData,
-  isBaseHintData,
   isValidIdentifierParam,
   isValidIdParam,
   isValidLookupQuery,
   isValidObject,
+  isValidString,
 } from "@src/utils/type.util";
 import { type Request, type Response } from "express";
 import { StatusCodes } from "http-status-codes";
@@ -54,14 +51,50 @@ export const create = async (req: Request, res: Response) => {
   }
 
   return res
-    .status(StatusCodes.OK)
+    .status(StatusCodes.CREATED)
     .json({ success: true, data: { message: `Hint created successfully.` } });
 };
 
 export const all = async (req: Request, res: Response) => {
-  const hints = await Hint.all();
+  const query = req.query;
 
-  return res.json({ hints });
+  let slug: string = "";
+
+  if (isValidObject(query) && isValidString(query.problem)) {
+    slug = query.problem;
+  }
+
+  const mappedHints = new Map<string, BaseHintData[]>();
+
+  if (slug) {
+    const problem = await getProblemByLookup(slug, "slug");
+
+    const hints = (await Hint.findByProblem(problem.id)) as BaseHintData[];
+
+    mappedHints.set(problem.slug, hints);
+
+    return res
+      .status(StatusCodes.OK)
+      .json({
+        success: true,
+        data: { hints: Object.fromEntries(mappedHints) },
+      });
+  } else {
+    const problems = (await Problem.all()) as BaseProblemData[];
+
+    for (const p of problems) {
+      const hint = (await Hint.findByProblem(p.id)) as BaseHintData[];
+
+      mappedHints.set(p.slug, hint);
+    }
+
+    return res
+      .status(StatusCodes.OK)
+      .json({
+        success: true,
+        data: { hints: Object.fromEntries(mappedHints) },
+      });
+  }
 };
 
 export const find = async (req: Request, res: Response) => {
