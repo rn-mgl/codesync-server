2b5addf9a9ee0b227f3cc66fefa0a67a82c69211
rn-mgl <reinb2ra@gmail.com>
2026-06-26 20:54:33 +0800

    stream tokens

--- src/controllers/cody.controller.ts
+++ src/controllers/cody.controller.ts
@@ -7,14 +7,29 @@ export const create = async (req: Request, res: Response) => {
 
   const ai = new GoogleGenAI({ apiKey: env.GEMINI_KEY });
 
-  const interact = await ai.interactions.create({
+  // for SSE
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "cache-control": "no-cache",
+    connection: "keep-alive",
+  });
+
+  const stream = await ai.interactions.create({
     model: "gemini-3.1-flash-lite",
     input: body.message,
     previous_interaction_id: body.interaction ?? null,
+    stream: true,
   });
 
-  return res.json({
-    success: true,
-    data: { output: interact.output_text, interaction: interact.id },
-  });
+  for await (const event of stream) {
+    if (event.event_type === "step.delta") {
+      if (event.delta.type === "text") {
+        // Correctly sends data over the line
+        res.write(`data: ${event.delta.text}\n\n`);
+      }
+    }
+  }
+
+  // end transaction
+  res.end();
 };
