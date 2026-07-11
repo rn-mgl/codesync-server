f3ab486a21442ce21d5e1069dd8ea8be203529f2
rn-mgl <reinb2ra@gmail.com>
2026-07-11 15:51:39 +0800

    change build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/**/*.ts"],
+  entry: ["src/server.ts", "src/services/worker.service.ts"],
   outDir: "dist",
   format: ["esm"],
   clean: true,
